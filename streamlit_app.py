1ec10fcd67e04779066f1259290bdcaebe883b7b
aufafreshikaaryani07-jpg <aufafreshikaaryani07@gmail.com>
2026-05-22 10:45:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Aplikasi Perkenalan Diri")
+st.title("🎈 Data Diri")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
