cef85f3f5d0cf0def9bf01ccaa0ecfa0a02dec43
aufafreshikaaryani07-jpg <aufafreshikaaryani07@gmail.com>
2026-05-22 10:42:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ st.title("🎈 Aplikasi Kimia")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+action = st.menu_button("Export", options=["Nama", "Asal", "Kelas"])
+if action == "Nama":
+    st.write("Exporting as Aufa Freshika")
+elif action == "Asal":
+    st.write("Exporting as Bogor")
+elif action == "Kelas":
+    st.write("Exporting as 1D")
