8c9845c23f218f35939ea0d21dafcbbf47d199d4
aufafreshikaaryani07-jpg <aufafreshikaaryani07@gmail.com>
2026-05-22 10:43:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Aplikasi Kimia")
+st.title("🎈 Aplikasi Perkenalan Diri")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
@@ -8,8 +8,8 @@ import streamlit as st
 
 action = st.menu_button("Halo", options=["Nama", "Asal", "Kelas"])
 if action == "Nama":
-    st.write("Exporting as Aufa Freshika")
+    st.write("Halo as Aufa Freshika")
 elif action == "Asal":
-    st.write("Exporting as Bogor")
+    st.write("Halo as Bogor")
 elif action == "Kelas":
-    st.write("Exporting as 1D")
+    st.write("Halo as 1D")
